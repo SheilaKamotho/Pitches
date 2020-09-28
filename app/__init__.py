b10bd25e7bb08bb818ef261f442c906717fcc0d3
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-28 17:12:49 +0300

    add authentification

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
-
+    
     app = Flask(__name__)
 
     # Creating the app configurations
@@ -22,7 +22,9 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+      
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     # # setting config
     # # from .request import configure_request
     # configure_request(app)
