037e5d49227ee72a994ef21146560be2a0b1c253
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-28 20:43:11 +0300

    add secret key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sheila:kamo2211@localhost/pitchy'
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
