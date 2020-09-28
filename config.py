f43462722b3e9b2d7b7880e4edb0314baf43ba24
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-28 08:58:31 +0300

    add html base structure

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config:
+    pass
+
+class ProdConfig(Config):
+    '''
+    Pruduction  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
